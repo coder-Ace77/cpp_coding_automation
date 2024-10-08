9d6d86523006842655171618698bd40165b115fb
Mohd Adil <adil7k777@gmail.com>
2024-10-08 15:37:06 +0530

    main v2 ready - optimised for performance

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import sys
+
 def split_file_by_delimiter(file_path, delimiter='$'):
     try:
         with open(file_path, 'r') as file:
@@ -11,26 +13,41 @@ def split_file_by_delimiter(file_path, delimiter='$'):
         print(f"An error occurred: {e}")
         return []
 
-out_arr = split_file_by_delimiter(file_path="outputx.txt")
-expected_arr = split_file_by_delimiter(file_path="expected.txt" , delimiter='\n')
+def split_string(content, delimiter='$'):
+    split_content = [part.strip() for part in content.split(delimiter) if part.strip()]
+    return split_content
 
-def split_string_by_newline(input_string):
+def split_string_only(input_string,delimiter='\n'):
     split_content = [line.rstrip() for line in input_string.split('\n') if line.strip()]
     return split_content
 
-j = 0
+def store(buffer):
+    fl = open("output.txt" , 'w')
+    fl.write(buffer)
 
+output_str = sys.stdin.read().strip()    
+expected_arr = split_file_by_delimiter(file_path="expected.txt", delimiter='\n')
+out_arr = split_string(output_str)
+
+storedStr = output_str.replace('$',' ')
+
+
+j = 0
 for i in range(len(out_arr)):
-    s = split_string_by_newline(out_arr[i])
+    s = split_string_only(out_arr[i])
     for line in s:
-        if expected_arr[j]==line:
-            j+=1
+        if expected_arr[j] == line:
+            j += 1
         else:
             print(f"Failed on test case {i+1}  : expected '{expected_arr[j]}'  , found '{line}'")
+            store(storedStr)
             exit()
 
-if(j!=len(expected_arr)):
+if j != len(expected_arr):
     print(f"Failed on test case {len(out_arr)}")
 else:
     print("All passed")
+store(storedStr)
+
+
 
--- script.sh
+++ script.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-g++ main.cpp -o main
-if [ $? -ne 0 ]; then
-    echo "Compilation failed"
-    exit 1
-fi
-
-./main < input.txt > output.txt
 sed -i 's|// cout<<\"\$\"<<endl;|cout<<\"\$\"<<endl;|' main.cpp
+current_hash=$(sha256sum main.cpp | awk '{ print $1 }')
 
-g++ main.cpp -o main
-if [ $? -ne 0 ]; then
-    echo "Compilation failed"
-    exit 1
+if [ -f last_hash.txt ]; then
+    last_hash=$(cat last_hash.txt)
+else
+    last_hash=""
 fi
 
-./main < input.txt > outputx.txt
+if [ "$current_hash" == "$last_hash" ]; then
+    echo "No changes detected in main.cpp. Skipping compilation."
+else
+    echo "Changes detected. Compiling main.cpp..."
+    g++ main.cpp -o main
+    if [ $? -ne 0 ]; then
+        echo "Compilation failed"
+        exit 1
+    fi
+    
+    echo "$current_hash" > last_hash.txt
+    echo "Compiled"
+fi
 
+./main < input.txt | python3 script.py
 if [ $? -ne 0 ]; then
     echo "Execution failed"
     exit 1
 fi
-
-python3 script.py
 
 sed -i 's|cout<<\"\$\"<<endl;|// cout<<\"\$\"<<endl;|' main.cpp
